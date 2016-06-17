0f92be08a5b456774d65f173827082e9c1186868
javi <javier.ferrer@student.ncirl.ie>
2016-06-17 17:55:58 +0000

    validate article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,16 +6,29 @@ class ArticlesController < ApplicationController
 
 
     def new
+      @article = Article.new
     end
 
+
     def create
-      @article = Article.new(params.require(:article).permit(:title, :text))
+      @article = Article.new(article_params)
      
-      @article.save
-      redirect_to @article
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end
+
     end
 
     def show
         @article = Article.find(params[:id])
     end
+    
+    private
+    
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
+
 end
